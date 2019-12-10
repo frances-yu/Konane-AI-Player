31913577fe24b005117689b64c31fa5b38382f78
Frances Yu <francesyu574@gmail.com>
2019-12-10 00:17:36 -0500

    Added Gameboard and Gamepiece

---
+++ Gamepiece.py
@@ -0,0 +1,24 @@
+# Gamepiece class object for Konane
+
+class Gamepiece():
+    def __init__(self, col, loc):
+        self.color = col
+        self.location = loc
+
+    def get_color(self):
+        print(self.color)
+
+    def get_location(self):
+        print(self.location)
+
+    def move_location(self, new_loc):
+        self.location = new_loc
+
+    def removed(self):
+        self.location = 'None'
+
+temp = Gamepiece('black', 'A1')
+temp.get_location()
+temp.get_color()
+temp.move_location('A3')
+temp.get_location()
