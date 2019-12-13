5efcc3a45a2d4d5304fa8b418a8ce9b408d8d202
Frances Yu <francesyu574@gmail.com>
2019-12-13 01:44:37 -0500

    Updates to main and RemotePlayer

--- RemotePlayer.py
+++ RemotePlayer.py
@@ -1,37 +1,34 @@
 # RemotePlayer.py
-
+# Contains all input streams and handling for remote player
 from Game import Move
 from Player import Player
 
-
 class RemotePlayer(Player):
-    def __init__(self, color, goes_first):
-        super().__init__(color, goes_first)
+    def __init__(self, name, is_bottom_left, tn):
+        super().__init__(name, is_bottom_left)
+        self.tn = tn
 
-    def get_color(self):
-        return self.color
-
-    def get_move(self, board):
-        # wait for move from server and output to move
-        try:
-            #Move[2:1]:[0:1]
-            move_tuple = tn.read_until(b"\n").decode('ASCII')[:-1]
-            move: Move = Move(move_tuple[0][0], move_tuple[0][1], move_tuple[1][0], move_tuple[1][1])
-        except:
-            move: Move = Move(-1, -1, -1, -1)
-        return move
-
-
-
-    def __init__(self, name, is_top_left):
-        super().__init__(name, is_top_left)
-
-    def get_move(self, board):
+    def get_move(self, board, prev_move):
         # Input move with format ((r1,c1),(r2,c2)) where r1,c1,r2,c2 are 0-indexed positions starting from top left
         # For first two moves, move formatted as (r1,c1)
         # For example, ((1,2),(1,4)) will move a piece at row 1, col 2 to a space at row 1, col 4
         # ((0,0),(0,0)) will take from the top left space on the first move
+
+        # ?Move(180000):
+        # [2:0]:[0:0]
+        # Move[2:0]:[0:0]
+        # Move[2:1]:[0:1]
+        # ?Move(162757):
+
         try:
+            q = tn.read_until(b"\n").decode('ASCII')[:-1]
+            time_left = q[6:]
+            time_left = time_left[:-2]
+
+
+
+
+
             move_tuple = eval(input("Enter move for " + self.name + ": "))
             move: Move = Move(move_tuple[0][0], move_tuple[0][1], move_tuple[1][0], move_tuple[1][1])
         except:
